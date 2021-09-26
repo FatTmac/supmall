1fbb830c1698da73aecb0a7ce2e66defac028a79
FatTmac <s814720555@163.com>
2021-09-26 21:23:06 +0800

    第二次修改

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="MainTabBar">
+    <tab-bar>
+       <tab-bar-item path='/home' >
+        <img slot="item-img" src="~assets/img/tabbar/home/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+        <tab-bar-item path='/category'>
+        <img slot="item-img" src="~assets/img/tabbar/category/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+        <tab-bar-item path='/shopcart'>
+        <img slot="item-img" src="~assets/img/tabbar/shopcart/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart/shopcart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+        <tab-bar-item path='/profile'>
+        <img slot="item-img" src="~assets/img/tabbar/profile/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar.vue'
+import TabBarItem from 'components/common/tabbar/TabBarItem.vue'
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
+

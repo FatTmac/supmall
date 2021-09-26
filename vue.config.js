0fee5d07cf6ba1477a6b1d8dbb8e718839e4e434
FatTmac <s814720555@163.com>
2021-09-26 16:56:49 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configurwebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+
+
+      }
+    }
+  }
+}

77c025f271acf7b59ba21258996d0fadae7f0af4
FatTmac <s814720555@163.com>
2021-09-28 22:35:45 +0800

    第五次修改

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -2,23 +2,23 @@
   <div id="MainTabBar">
     <tab-bar>
        <tab-bar-item path='/home' >
-        <img slot="item-img" src="~assets/img/tabbar/home/home.svg" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbar/home/home_active.svg" alt="">
+        <img slot="item-img" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
         <tab-bar-item path='/category'>
-        <img slot="item-img" src="~assets/img/tabbar/category/category.svg" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbar/category/category_active.svg" alt="">
+        <img slot="item-img" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
         <tab-bar-item path='/shopcart'>
-        <img slot="item-img" src="~assets/img/tabbar/shopcart/shopcart.svg" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart/shopcart_active.svg" alt="">
+        <img slot="item-img" src="~assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
         <tab-bar-item path='/profile'>
-        <img slot="item-img" src="~assets/img/tabbar/profile/profile.svg" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbar/profile/profile_active.svg" alt="">
+        <img slot="item-img" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,42 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav">
       <div slot="center">购物车</div>
     </nav-bar>
   <home-swiper :banners="banners"></home-swiper>
   <recommend-view :recommends="recommends"></recommend-view>
+  <feature-view></feature-view>
+  <tab-control class="tab-control" :titles='["流行","新款","精选"]'></tab-control>
+  <ul>
+    
+
+    <li></li>
+    <li></li>
+        <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+        <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
   </div>
 </template>
 
@@ -12,7 +44,12 @@
 import NavBar from 'components/common/navbar/NavBar.vue'
 import HomeSwiper from './childComps/HomeSwiper.vue'
 import RecommendView from './childComps/RecommendView.vue'
-import { getHomeMultidata } from 'network/home.js'
+import FeatureView from './childComps/FeatureView.vue'
+import TabControl from 'components/content/tabControl/TabControl.vue'
+
+
+import { getHomeMultidata,getHomeGoods } from 'network/home.js'
+
 
 
 
@@ -21,28 +58,70 @@ export default {
   data() {
     return {
       banners:[],
-      recommends:[]
+      recommends:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]}
+
+      }
     }
   },
   components: {
      NavBar,
     HomeSwiper,
-    RecommendView ,
+    RecommendView,
+    FeatureView,
+    TabControl ,
     
      },
   created() {
-    getHomeMultidata().then(res=>{
+    // 请求多个数据
+    this.getHomeMultidata()
+
+    // 请求商品数据
+    // this.getHomeGoods('pop')
+    // this.getHomeGoods('new')
+    // this.getHomeGoods('sell')
+   
+  },
+  methods: {
+      getHomeMultidata(){
+      getHomeMultidata().then(res=>{
       this.banners=res.data.banner.list;
       this.recommends=res.data.recommend.list;
-
     })
-  },
+    },
+    getHomeGoods(type){
+        const page = this.goods[type].page + 1
+       getHomeGoods(type,page).then(res=>{
+      // for (let v in res.data.list) {
+      //   this.goods[type].lsit.push(v)
+      // // }
+      // this.goods[type].list.push(...res.data.list)
+      // this.goods[type].page+=1
+      console.log(res.data);
+    })
+    }
+  }
 }
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
   .home-nav{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
     background-color: var(--color-tint);
     color: #fff;
+    z-index: 999,
+  }
+  .tab-control{
+    position:sticky;
+    top: 44px;
   }
 </style>
